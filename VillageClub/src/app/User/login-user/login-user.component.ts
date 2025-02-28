23d6a3d8758bded63505cf19c4ad56d1247d6397
pidchanard <pchanard.m@ku.th>
2025-02-28 14:36:40 +0700

    fix home page

--- VillageClub/src/app/User/login-user/login-user.component.ts
+++ VillageClub/src/app/User/login-user/login-user.component.ts
@@ -4,15 +4,18 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-user',
   templateUrl: './login-user.component.html',
-  styleUrl: './login-user.component.css'
+  styleUrls: ['./login-user.component.css']
 })
 export class LoginUserComponent {
   constructor(private router: Router) {}
 
   goHome() {
-    this.router.navigate(['/home']);
+    this.router.navigate(['/home']).then(() => {
+      window.location.reload(); // รีเฟรชหลังจากเปลี่ยนหน้า
+    });
   }
-  goReg(){
+
+  goReg() {
     this.router.navigate(['/reg']);
   }
 }
